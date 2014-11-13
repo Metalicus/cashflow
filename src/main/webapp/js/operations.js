89516db4796a9fb6a6ac718129f0977b890ba035
Metalicus <alex.metalicus@gmail.com>
2014-11-13 12:33:39 +0300

    tabs two-way-binding

--- src/main/webapp/js/operations.js
+++ src/main/webapp/js/operations.js
@@ -29,6 +29,30 @@
         }
     });
 
+    operations.directive('typeTabs', function () {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function (scope, element, attrs, ngModel) {
+                if (!ngModel) return; // do nothing if no ng-model
+
+                // select default tab
+                scope.selTab(ngModel.$viewValue);
+
+                element.on("click", function () {
+                    var currentType = scope.getCurrentType();
+                    if (currentType !== ngModel.$viewValue) { // update only if new value
+                        ngModel.$setViewValue(currentType);
+                    }
+                });
+
+                ngModel.$render = function () { // select new tab then model is changed
+                    scope.selTab(ngModel.$viewValue);
+                };
+            }
+        }
+    });
+
     operations.controller('OperationsCtrl', ['$scope', '$http', '$modal', function ($scope, $http, $modal) {
         $scope.gridOptions = {
             enableRowHeaderSelection: false,
@@ -92,138 +116,109 @@
             });
     }]);
 
-    operations.service('tabService', function () {
-        this.type = null;
-        this.editMode = false;
-        this.setType = function (newType) {
-            this.type = newType;
-        };
-        this.enableEditMode = function () {
-            this.editMode = true;
-        };
-        this.disableEditMode = function () {
-            this.editMode = false;
-        }
-    });
-
-    operations.controller('TabController', ['$scope', 'tabService', function ($scope, tabService) {
+    operations.controller('TabController', ['$scope', function ($scope) {
         $scope.tabs = [
             {type: 'OUTCOME', disabled: false, active: true},
             {type: 'TRANSFER', disabled: false, active: false},
             {type: 'INCOME', disabled: false, active: false}
         ];
 
-        $scope.$watch(function () {
-                return tabService.type;
-            },
-            function (newVal) {
-                if (newVal) {
-                    for (var i = 0; i < $scope.tabs.length; i++) {
-                        $scope.tabs[i].active = $scope.tabs[i].type === newVal;
-                    }
+        $scope.getCurrentType = function () {
+            var currentType = null;
+            for (var i = 0; i < $scope.tabs.length; i++) {
+                if ($scope.tabs[i].active) {
+                    currentType = $scope.tabs[i].type;
+                    break;
                 }
-            }, true);
+            }
 
-        $scope.$watch(function () {
-                return tabService.editMode;
-            },
-            function (newVal) {
-                for (var i = 0; i < $scope.tabs.length; i++) {
-                    $scope.tabs[i].disabled = newVal;
-                }
-            }, true);
+            return currentType;
+        };
+
+        $scope.selTab = function (type) {
+            for (var i = 0; i < $scope.tabs.length; i++) {
+                $scope.tabs[i].active = type === $scope.tabs[i].type;
+            }
+        };
     }]);
 
-    operations.controller('OperationEditCtrl', ['$scope', '$modalInstance', '$http', 'tabService', 'id',
-        function ($scope, $modalInstance, $http, tabService, id) {
-            // operation model
-            $scope.model = {
-                id: id,
-                type: 'OUTCOME',
-                date: new Date(),
-                amount: null,
-                moneyWas: null,
-                moneyBecome: null,
-                account: {},
-                currency: {},
-                category: {}
-            };
+    operations.controller('OperationEditCtrl', ['$scope', '$modalInstance', '$http', 'id', function ($scope, $modalInstance, $http, id) {
+        // operation model
+        $scope.model = {
+            id: id,
+            type: 'OUTCOME',
+            date: new Date(),
+            amount: null,
+            moneyWas: null,
+            moneyBecome: null,
+            account: {},
+            currency: {},
+            category: {}
+        };
 
-            tabService.setType($scope.model.type);
-            tabService.disableEditMode();
+        // ui-select models
+        $scope.account = {};
+        $scope.currency = {};
+        $scope.category = {};
 
-            // ui-select models
-            $scope.account = {};
-            $scope.currency = {};
-            $scope.category = {};
+        $scope.updateMoney = function () {
 
-            $scope.changeType = function (newType) {
-                $scope.model.type = newType;
-                tabService.setType(newType);
-            };
+        };
 
-            $scope.updateMoney = function () {
+        $scope.accountSelect = function (model) {
+            $scope.model.account = model;
+            $scope.model.moneyWas = model["balance"];
+        };
 
-            };
+        $scope.categorySelect = function (model) {
+            $scope.model.category = model;
+        };
 
-            $scope.accountSelect = function (model) {
-                $scope.model.account = model;
-                $scope.model.moneyWas = model["balance"];
-            };
+        $scope.currencySelect = function (model) {
+            $scope.model.currency = model;
+        };
 
-            $scope.categorySelect = function (model) {
-                $scope.model.category = model;
-            };
+        $scope.submit = function () {
+            $modalInstance.dismiss('ok');
+        };
 
-            $scope.currencySelect = function (model) {
-                $scope.model.currency = model;
-            };
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
 
-            $scope.submit = function () {
-                $modalInstance.dismiss('ok');
-            };
+        $http.get('action/account/list')
+            .then(function (response) {
+                $scope.accounts = response.data;
+            });
 
-            $scope.cancel = function () {
-                $modalInstance.dismiss('cancel');
-            };
+        $http.get('action/currency/list')
+            .then(function (response) {
+                $scope.currencies = response.data;
+            });
 
-            $http.get('action/account/list')
-                .then(function (response) {
-                    $scope.accounts = response.data;
+        $http.get('action/category/list')
+            .then(function (response) {
+                $scope.categories = response.data;
+            });
+
+        if (id !== null) {
+            $http.get('action/operation/get/' + id)
+                .success(function (data) {
+                    //model
+                    $scope.model.type = data.type;
+                    $scope.model.date = new Date(data.date);
+                    $scope.model.amount = data.amount;
+                    $scope.model.moneyWas = data.moneyWas;
+                    $scope.model.moneyBecome = data.moneyBecome;
+                    $scope.model.account = data.account;
+                    $scope.model.currency = data.currency;
+                    $scope.model.category = data.category;
+
+                    //ui-select
+                    $scope.account.selected = data.account;
+                    $scope.currency.selected = data.currency;
+                    $scope.category.selected = data.category;
                 });
-
-            $http.get('action/currency/list')
-                .then(function (response) {
-                    $scope.currencies = response.data;
-                });
-
-            $http.get('action/category/list')
-                .then(function (response) {
-                    $scope.categories = response.data;
-                });
-
-            if (id !== null) {
-                $http.get('action/operation/get/' + id)
-                    .success(function (data) {
-                        //model
-                        $scope.model.type = data.type;
-                        $scope.model.date = new Date(data.date);
-                        $scope.model.amount = data.amount;
-                        $scope.model.moneyWas = data.moneyWas;
-                        $scope.model.moneyBecome = data.moneyBecome;
-                        $scope.model.account = data.account;
-                        $scope.model.currency = data.currency;
-                        $scope.model.category = data.category;
-
-                        //ui-select
-                        $scope.account.selected = data.account;
-                        $scope.currency.selected = data.currency;
-                        $scope.category.selected = data.category;
-
-                        //tabs settings
-                        $scope.changeType($scope.model.type);
-                        tabService.enableEditMode();
-                    });
-            }
-        }]);
+        }
+    }]);
 })();
