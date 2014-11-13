ae158ecfdbd7294a9b8d99f7cc63c66e500fe3da
Metalicus <alex.metalicus@gmail.com>
2014-11-13 14:47:22 +0300

    operation form design

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,6 +2,17 @@
     var cashFlow = angular.module('cashFlow', ['cashflow-operations', 'cashflow-account', 'cashflow-currency',
         'cashflow-category', 'ngRoute', 'ui.grid', 'ui.grid.selection', 'ui.grid.cellNav', 'ui.bootstrap', 'ui.select']);
 
+    // fix for ui-bootstrap problem with angularjs 1.3
+    cashFlow.directive('tooltip', function () {
+        return {
+            restrict: 'EA',
+            link: function (scope, element, attrs) {
+                attrs.tooltipTrigger = attrs.tooltipTrigger;
+                attrs.tooltipPlacement = attrs.tooltipPlacement || 'top';
+            }
+        }
+    });
+
     cashFlow.config(function (uiSelectConfig) {
         uiSelectConfig.theme = 'bootstrap';
     });
