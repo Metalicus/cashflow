52aaf0cab65fff562a28d7855ea6e562ea75656d
Metalicus <alex.metalicus@gmail.com>
2014-12-12 16:16:10 +0300

    grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,92 @@ module.exports = function (grunt) {
     require('time-grunt')(grunt);
 
     grunt.initConfig({
+        cashflow: {
+            dist: 'dist'
+        },
         karma: {
             unit: {
                 configFile: 'src/test/javascript/karma.conf.js',
                 singleRun: true
+            }
+        },
+        useminPrepare: {
+            html: 'src/main/webapp/**/*.html',
+            options: {
+                dest: '<%= cashflow.dist %>'
+            }
+        },
+        usemin: {
+            html: ['<%= cashflow.dist %>/**/*.html'],
+            css: ['<%= cashflow.dist %>/css/**/*.css'],
+            options: {
+                assetsDirs: ['<%= cashflow.dist %>/**/'],
+                dirs: ['<%= cashflow.dist %>']
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeCommentsFromCDATA: true,
+                    collapseWhitespace: true,
+                    collapseBooleanAttributes: true,
+                    conservativeCollapse: true,
+                    removeAttributeQuotes: true,
+                    removeRedundantAttributes: true,
+                    useShortDoctype: true,
+                    removeEmptyAttributes: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= cashflow.dist %>',
+                    src: ['*.html', 'template/**/*.html'],
+                    dest: '<%= cashflow.dist %>'
+                }]
+            }
+        },
+        copy: {
+            dist: {
+                expand: true,
+                dot: true,
+                cwd: 'src/main/webapp',
+                dest: '<%= cashflow.dist %>',
+                src: [
+                    '*.html',
+                    'template/*.html',
+                    'WEB-INF/*.xml'
+                ]
+            },
+            bootstrap: {
+                expand: true,
+                flatten: true,
+                cwd: 'src/main/webapp/bower_components/bootstrap/fonts',
+                src: [
+                    '**/*.*'
+                ],
+                dest: '<%= cashflow.dist %>/fonts'
+            },
+            grid: {
+                expand: true,
+                flatten: true,
+                cwd: 'src/main/webapp/bower_components/angular-ui-grid',
+                src: [
+                    'ui-grid.eot',
+                    'ui-grid.svg',
+                    'ui-grid.ttf',
+                    'ui-grid.woff'
+                ],
+                dest: '<%= cashflow.dist %>/css'
+            }
+        },
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%= cashflow.dist %>'
+                    ]
+                }]
             }
         }
     });
@@ -17,7 +99,21 @@ module.exports = function (grunt) {
         'karma'
     ]);
 
+    grunt.registerTask('build', [
+        'clean:dist',
+        'useminPrepare',
+        'concat',
+        'copy:dist',
+        'copy:bootstrap',
+        'copy:grid',
+        'uglify',
+        'usemin',
+        'cssmin',
+        'htmlmin'
+    ]);
+
     grunt.registerTask('default', [
-        'test'
+        'test',
+        'build'
     ]);
 };
